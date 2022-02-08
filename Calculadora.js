128d96e0c50b2007c4bbc7f7f12e02317e8de405
zeroprotec <69656762+zeroprotec@users.noreply.github.com>
2022-02-08 10:24:35 -0500

    Calculadora Iva
    Calcula el iva y devuelve un valor con iva incluido

---
+++ Calculadora.js
@@ -0,0 +1,7 @@
+let iva = 1.19; // Variable Iva
+function calculadoriva(precio) {
+    var ivaproducto = precio * iva;
+    console.log("El precio de su producto mas iva es " + ivaproducto);
+    // funcion calcular el Iva
+
+}
